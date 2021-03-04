ae8dd04c8a6f1e887bd417af118246836d5d9101
lakiert <aleksandra.lakiert@gmail.com>
2021-03-04 14:31:52 +0100

    update1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# zad1
+from math import *
 
+# zad1
+print('zadanie 1: \n')
 liczba1 = 5
 liczba2 = 10
 
@@ -20,3 +22,64 @@ print(napis1)
 print(napis2)
 print(liczba5)
 print(liczba6)
+
+# zad2
+print('\n zadanie 2: \n')
+a, b = 10, 5
+dodawanie = a + b
+odejmowanie = a - b
+mnozenie = a * b
+dzielenie = a / b
+print(dodawanie,odejmowanie,mnozenie,dzielenie)
+
+# zad3
+print('\n zadanie 3: \n')
+
+a += 1
+print(a)
+a -= 1
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a **= 2
+print(a)
+a %= 2
+print(a)
+
+# zad4
+print('\n zadanie 4: \n')
+
+print(pow(e,10))
+print(pow(log(5+pow(sin(8),2)),1/6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
