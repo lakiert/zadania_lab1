214abb000eac9bef8ac0b6990c28b6468cfc0eb3
lakiert <aleksandra.lakiert@gmail.com>
2021-03-04 13:58:58 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+liczba1 = 5
+liczba2 = 10
+
+liczba3 = 3.5
+liczba4 = 12.7892
+
+napis1 = 'Ala ma kota'
+napis2 = 'Ola ma psa'
+
+liczba5 = 5+2j
+liczba6 = 10+12j
+
+print(liczba1)
+print(liczba2)
+print(liczba3)
+print(liczba4)
+print(napis1)
+print(napis2)
+print(liczba5)
+print(liczba6)
